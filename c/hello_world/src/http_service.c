28aadb05297dc1ea8deca8a39706f3f083fa2449
Michael J Erwin <michaeljameserwin@gmail.com>
2023-11-04 09:03:46 -0400

    C: hello_world error cases

--- c/hello_world/src/http_service.c
+++ c/hello_world/src/http_service.c
@@ -2,6 +2,7 @@
 #include "fiobj_json.h"
 #include "fiobj_str.h"
 #include "fiobject.h"
+#include "http.h"
 #include <string.h>
 #define FIO_INCLUDE_STR
 #include "fio.h"
@@ -29,31 +30,36 @@ static void on_http_request(http_s *h) {
     char *hello = strtok(path.data, "/");
     // truncate name or put str on heap so we can have variable length
     char *name = strtok(NULL, "/");
-    puts(hello);
     puts(name);
-    char greeting_str[50] = "Hello ";
-    strcat(greeting_str, name);
-    puts(greeting_str);
 
-    // {"greeting": "Hello <name>"}
-    http_set_header(h, HTTP_HEADER_CONTENT_TYPE, http_mimetype_find("json", 4));
+    if (name != NULL) {
+      char greeting_str[50] = "Hello ";
+      strcat(greeting_str, name);
+      puts(greeting_str);
 
-    FIOBJ json;
-    /* create a new Hash to be serialized for every request */
-    FIOBJ hash = fiobj_hash_new2(1);
-    FIOBJ key = fiobj_str_new("greeting", 8);
-    FIOBJ greeting = fiobj_str_new(greeting_str, strlen(greeting_str));
-    fiobj_hash_set(hash, key, greeting);
+      // {"greeting": "Hello <name>"}
+      http_set_header(h, HTTP_HEADER_CONTENT_TYPE, http_mimetype_find("json", 4));
 
-    json = fiobj_obj2json(hash, 0);
-    fio_str_info_s tmp = fiobj_obj2cstr(json);
+      FIOBJ json;
+      /* create a new Hash to be serialized for every request */
+      FIOBJ hash = fiobj_hash_new2(1);
+      FIOBJ key = fiobj_str_new("greeting", 8);
+      FIOBJ greeting = fiobj_str_new(greeting_str, strlen(greeting_str));
+      fiobj_hash_set(hash, key, greeting);
 
-    http_send_body(h, tmp.data, tmp.len);
+      json = fiobj_obj2json(hash, 0);
+      fio_str_info_s tmp = fiobj_obj2cstr(json);
 
-    fiobj_free(hash);
-    fiobj_free(json);
-    fiobj_free(key);
-    fiobj_free(greeting);
+      http_send_body(h, tmp.data, tmp.len);
+
+      fiobj_free(hash);
+      fiobj_free(json);
+      fiobj_free(key);
+      fiobj_free(greeting);
+      fio_free(tmp.data);
+    } else {
+      http_send_error(h, 400);
+    }
   } else if (strcmp(path.data, "/goodbye") == 0
              && strcmp(method.data, "POST") == 0) {
     // get json body.name
@@ -66,14 +72,13 @@ static void on_http_request(http_s *h) {
     if (cJSON_IsString(name)) {
       strcat(greeting_str, name->valuestring);
     } else {
-      strcat(greeting_str, "wtf you doing");
+      http_send_error(h, 400);
     }
 
     http_send_body(h, greeting_str, strlen(greeting_str));
     fio_free(body.data);
   } else {
-    puts(strcmp(path.data, "/hello") == 0 ? "true" : "false");
-    puts(strcmp(method.data, "GET") == 0 ? "true" : "false");
+    http_send_error(h, 404);
   }
 
   // probably free stuff?
