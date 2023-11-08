47d8c09ea5cdf615e0670202b260bd62eb58e0bb
Michael J Erwin <michaeljameserwin@gmail.com>
2023-11-07 22:50:02 -0500

    Zig: more http work

--- zig/README.md
+++ zig/README.md
@@ -1,7 +1,10 @@
 # Zig
 
-[Reference](https://ziglang.org)
+[Home Page](https://ziglang.org)
+[Zig Language Reference](https://ziglang.org/documentation/master/)
+[Zig Standard Library Docs](https://ziglang.org/documentation/master/std/#A;std)
 [ziglearn "book"](https://ziglearn.org)
+[Ziglings](https://ziglings.org/)
 
 ## Dev Env
 
@@ -16,4 +19,12 @@ Add that directory to your path `echo 'export PATH=$PATH:~/path/to/zig' >> .bash
 ### LSP
 [zls](https://github.com/zigtools/zls/wiki/Installation#prebuilt-binaries) - I just downloaded the prebuilt binary and put it in the same dir as my zig install. Eglot found it without issue.
 
+### Package Management
+Zon is the Zig package manager. It currently doesn't have a cli but works from the `build.zig.zon` file in project root to install dependencies.
+
 ### Issues
+The install process wasn't perfectly smooth, but it wasn't difficult either.
+
+## Packages
+[Zap](https://github.com/zigzap/zap) - Backend web micro framework. Wraps facil.io (which we used for C)
+> Note: We did have issues with Zap's latest pre-release not being compatible with Zig 0.12, thanks to chatter [Southporter](https://github.com/zigzap/zap/tree/master) we were able to patch our cached version of Zap for compatibility without downgrading our Zig toolchain.
--- zig/hello_world/src/main.zig
+++ zig/hello_world/src/main.zig
@@ -1,10 +1,12 @@
 const std = @import("std");
+const json = std.json;
 const zap = @import("zap");
 
 pub fn main() !void {
+    try setup_routes(std.heap.page_allocator);
     var listener = zap.SimpleHttpListener.init(.{
         .port = 3000,
-        .on_request = on_request,
+        .on_request = dispatch_routes,
         .log = true,
     });
 
@@ -19,7 +21,31 @@ pub fn main() !void {
     });
 }
 
-fn on_request(r: zap.SimpleRequest) void {
+var routes: std.StringHashMap(zap.SimpleHttpRequestFn) = undefined;
+
+fn setup_routes(a: std.mem.Allocator) !void {
+    routes = std.StringHashMap(zap.SimpleHttpRequestFn).init(a);
+    // GET `/hello` returns text response of "Hello World!"
+    try routes.put("/hello", hello);
+    // GET `/hello/<name>` returns json response of `{"greeting": "Hello <name>!"}`
+    try routes.put("/hello/meta", hello_json);
+    // POST `/goodbye` takes a json payload of `{"name":string}` and returns text response of "Goodbye <name>"
+    try routes.put("/goodbye", goodbye);
+}
+
+fn dispatch_routes(r: zap.SimpleRequest) void {
+    if (r.path) |the_path| {
+        if (routes.get(the_path)) |handler| {
+            handler(r);
+            return;
+        }
+    }
+
+    r.setStatus(.not_found);
+    r.sendBody("Not Found") catch return;
+}
+
+fn hello(r: zap.SimpleRequest) void {
     if (r.path) |the_path| {
         std.log.debug("PATH: {s}", .{the_path});
     }
@@ -29,6 +55,36 @@ fn on_request(r: zap.SimpleRequest) void {
     }
 
     r.sendBody("Hello World!") catch |err| {
+        std.log.debug("We got an err: {}", .{err});
+    };
+}
+
+fn hello_json(r: zap.SimpleRequest) void {
+    if (r.path) |the_path| {
+        std.log.debug("PATH: {s}", .{the_path});
+    }
+
+    if (r.query) |the_query| {
+        std.log.debug("QUERY: {s}", .{the_query});
+    }
+    const name = "meta";
+    r.sendJson("Hello " ++ name ++ "!") catch |err| {
+        std.log.debug("We got an err: {}", .{err});
+    };
+}
+
+fn goodbye(r: zap.SimpleRequest) void {
+    r.parseBody() catch |err| {
+        r.sendError(err, 400);
+        return;
+    };
+
+    const jsonResult = json.parseFromSlice(r.body, std.heap.page_allocator);
+
+    const name = jsonResult.root["name"].string;
+    defer name.deinit();
+
+    r.sendBody("Goodbye " ++ name) catch |err| {
         std.log.debug("We got an err: {}", .{err});
     };
 }
