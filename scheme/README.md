f3cd178f25dcd3483a3dd749af27c3b804e43268
Michael J Erwin <michaeljameserwin@gmail.com>
2023-11-09 23:36:07 -0500

    Scheme: hello_world http api complete

--- scheme/README.md
+++ scheme/README.md
@@ -1,6 +1,29 @@
-#https://www.gnu.org/software/guile/manual/guile.html#API-Reference Scheme
+# Scheme
 
 [Scheme.org](https://www.scheme.org/)
 [Reference](https://www.gnu.org/software/guile/manual/guile.html#API-Reference)
 [Scheme Primer](https://spritely.institute/static/papers/scheme-primer.html)
 [Guile](https://www.gnu.org/software/guile) - the scheme implementation we chose
+
+## Dev Env
+
+Emacs comes with `scheme-mode` built-in.
+
+### Compiler/Interpreter
+We chose the Guile implementation as it is a good beginner friendly implementation with manuals, docs, and a nice website (link in bio, errr 7 lines up). It was already installed on my system (I don't remember how or when) but on MacOS you can just `brew install guile`.
+
+### REPL
+Running `guile` with no args opens up a REPL. Being used to the very nice clojure REPL, this one felt a little clunky but it's usable.
+
+### LSP
+In common Lisp fashion, the REPL gives the emacs buffer information about your code and powers most things an LSP would. But for those outside of the emacs world, there is this that I didn't bother trying to install:
+[scheme-lsp-server](https://codeberg.org/rgherdt/scheme-lsp-server)
+
+### Package Management
+There's no package manager but I found the install process for Guile modules to be well-documented and straightforward with established conventions for configuration and linking that made using an external package easy.
+
+### Issues
+Most tools recommend installing with [Guix](https://guix.gnu.org), which seems like a good option if you're on GNU/Linux, but isn't supported on MacOS so we went without some tools that probably could've helped early on (like an LSP, for example)
+
+## Packages
+[guile-json](https://github.com/aconchillo/guile-json)
--- scheme/hello_world/server.scm
+++ scheme/hello_world/server.scm
@@ -1,7 +1,15 @@
-(use-modules (web server)
+(define-module (meta server))
+(use-modules (ice-9 iconv)
+             (json)
+             (web server)
              (web request)
              (web response)
              (web uri))
+
+(define (bad-request request)
+  (values (build-response #:code 400)
+          (string-append "Bad request on: "
+                         (uri->string (request-uri request)))))
 
 (define (not-found request)
   (values (build-response #:code 404)
@@ -11,22 +19,27 @@
 (define (request-path-components request)
   (split-and-decode-uri-path (uri-path (request-uri request))))
 
-;; GET `/hello` returns text response of "Hello World!"
+;; Get `/hello` returns text response of "Hello World!"
 (define (hello-world-handler request request-body)
   (values '((content-type . (text/plain)))
           "Hello World!"))
 
 ;; GET `/hello/<name>` returns json response of `{"greeting": "Hello <name>!"}`
 (define (hello-name-handler request request-body)
-  (let ((name (car (cdr (request-path-components request)))))
-    (values '((content-type . (text/plain)))
-          (string-append "Hello " name "!"))))
+  (let* ((name (car (cdr (request-path-components request))))
+         (greeting (string-append "Hello " name "!"))
+         (res (assoc-set! '() "greeting" greeting)))
+    (values '((content-type . (application/json)))
+            (scm->json-string res))))
 
 ;; POST `/goodbye` takes a json payload of `{"name":string}` and returns text response of "Goodbye <name>"
 (define (goodbye-handler request request-body)
-  (let ((name "meta"))
-    (values '((content-type . (text/plain)))
-            (string-append "Goodbye " name))))
+  (let* ((body (json-string->scm (bytevector->string request-body "utf-8")))
+         (name (assoc-ref body "name")))
+    (if (string? name)
+        (values '((content-type . (text/plain)))
+                (string-append "Goodbye " name))
+        (bad-request request))))
 
 (define (handler request request-body)
   (let ((path-parts (request-path-components request))
@@ -48,4 +61,5 @@
 
      (else (not-found request)))))
 
+(display "Starting server on localhost:8080")
 (run-server handler)
